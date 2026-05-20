d18b9f8a179ccefbbeb8bb1f52463b5ca5c7027b
mehul5277 <mehul5277@hotmail.com>
2026-05-20 15:54:46 -0400

    update them

--- src/app/public/pages/cv/cv.component.ts
+++ src/app/public/pages/cv/cv.component.ts
@@ -8,11 +8,12 @@ import { IndustriesComponent } from "../industries/industries.component";
 @Component({
   selector: 'app-cv',
   standalone: true,
-  imports: [HttpClientModule, SectionComponent, IndustriesComponent, JsonPipe],
+  imports: [HttpClientModule, SectionComponent, IndustriesComponent],
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.scss'
 })
 export class CVComponent implements OnInit {
+  tickerItems: any | undefined;
   SectionType = SectionType;
   linkedIn = `https://linkedin.com/in/mehul4ca`;
   cv: any = {
@@ -23,13 +24,16 @@ export class CVComponent implements OnInit {
       "email": "mehul****@hotmail.com",
       "phone": "(912) 555-4321",
       "headline": "AI-Powered Software Development",
-      "summary": "Your Ultimate Partner for World-Class Software Development, IT Consulting and Digital Transformation.",
+      "summary": "Turning Ideas into Secure IT Solutions.",
       "about": [
+        "Your Ultimate Partner for World-Class Software Development, IT Consulting and Digital Transformation.",
+        "Turning Ideas into Secure IT Solutions.",
         "Integrating heterogeneous enterprise systems, using diverse platforms, frameworks, design approaches, architectural styles, programming languages and cloud computing technology. Expert in mission-oriented technology solutions & dedicated to delivering technology solution.",
         "With over 10 years of experience in software development, I specialize in creating innovative solutions that drive business success. My expertise spans across various industries, including finance, healthcare, and e-commerce. I am passionate about leveraging the latest technologies to deliver high-quality software that meets the unique needs of each client."],
       "location": { "city": "San Francisco", "countryCode": "US" },
       "picture": "assets/image/dp.jpg",
-      "profiles": [{ "network": "LinkedIn", "url": "https://linkedin.com/in/mehul4ca" }]
+      "profiles": [{ "network": "LinkedIn", "url": "https://linkedin.com/in/mehul4ca" }],
+      "bg": "assets/image/hero-banner-1.webp"
     },
     "work": [{
       "name": "Company ABC",
@@ -59,6 +63,9 @@ export class CVComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dataService.getData("services").subscribe(data => this.services = data.services.slice(0, 10));
+    this.dataService.getData("services").subscribe(data => {
+      this.services = data.services.slice(0, 6);
+      this.tickerItems = [...this.services];
+    });
   }
 }
