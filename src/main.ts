ac2c035ae2bf6661530464025b3ecfdc715b4a39
mehul5277 <mehul5277@hotmail.com>
2026-05-22 00:36:39 -0400

    add ms clarity

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import Clarity from '@microsoft/clarity';
+
+// Replace with your actual project ID
+const clarityId = "wuwlr4ugmv";
+
+// Optional: Only run in production environment
+// if (environment.production) { ... }
+Clarity.init(clarityId);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
 
 if (environment.production) {
+  Clarity.event("custom-production-event");
   window.console.log = () => { };
 }
