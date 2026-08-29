73a01c3493ad24a079df22932896cd14a64cfa8e
mehul5277 <mehul5277@hotmail.com>
2026-08-29 15:02:23 -0400

    update ui

--- src/app/admin/admin-navbar/admin-navbar.component.ts
+++ src/app/admin/admin-navbar/admin-navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-admin-navbar',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './admin-navbar.component.scss'
 })
 export class AdminNavbarComponent {
+  private authService: AuthService = inject(AuthService);
 
+  logout() {
+    this.authService.logout();
+  }
 }
